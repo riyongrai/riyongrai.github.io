8a85e4441d84a06d4233a15d5aa1b4c7cd9bb822
Riyong <rairiyong@gmail.com>
2024-06-24 21:30:29 -0400

    changed text

--- js/main.js
+++ js/main.js
@@ -60,43 +60,42 @@ Menu
 	init();
 
 })();
-
-
 window.onload = function() {
-    var name = prompt("Hey there! Enter your name:");
+	var name = prompt("Hey there! Enter your name:");
 
-    // Convert name to uppercase
-    name = name ? name.toUpperCase() : "VISITOR";
+	// Convert name to uppercase
+	name = name ? name.toUpperCase() : "VISITOR";
 
-    var date = new Date();
-    var hour = date.getHours();
-    var greeting;
+	var date = new Date();
+	var hour = date.getHours();
+	var greeting;
 
-    if (hour >= 0 && hour < 12) {
-        greeting = "Good Morning";
-    } else if (hour >= 12 && hour < 18) {
-        greeting = "Good Afternoon";
-    } else {
-        greeting = "Good Evening";
-    }
+	if (hour >= 0 && hour < 12) {
+		greeting = "Good Morning";
+	} else if (hour >= 12 && hour < 18) {
+		greeting = "Good Afternoon";
+	} else {
+		greeting = "Good Evening";
+	}
 
-    // Array of humorous messages
-    var humorousMessages = [
-        "Prepare yourself, " + name + "! The adventure awaits!",
-        "Hold on tight, " + name + "! You're about to enter a world of wonders!",
-        "Congratulations, " + name + "! You've stumbled upon the treasure trove!",
-        "Welcome, esteemed guest " + name + "! Get ready for some serious fun!",
-        "Ahoy there, " + name + "! You've just stepped into the land of infinite possibilities!"
-    ];
+	// Array of humorous messages
+	var humorousMessages = [
+		`Prepare yourself, <span style="color: blue; font-weight: bold;">${name}</span>! The adventure awaits!`,
+		`Hold on tight, <span style="color: blue; font-weight: bold;">${name}</span>! You're about to enter a world of wonders!`,
+		`Congratulations, <span style="color: blue; font-weight: bold;">${name}</span>! You've stumbled upon the treasure trove!`,
+		`Welcome, esteemed guest <span style="color: blue; font-weight: bold;">${name}</span>! Get ready for some serious fun!`,
+		`Ahoy there, <span style="color: blue; font-weight: bold;">${name}</span>! Let's set sail on the seas of imagination!`
+	];
 
-    // Select a random humorous message
-    var randomIndex = Math.floor(Math.random() * humorousMessages.length);
-    var humorousMessage = humorousMessages[randomIndex];
+	// Select a random humorous message
+	var randomIndex = Math.floor(Math.random() * humorousMessages.length);
+	var humorousMessage = humorousMessages[randomIndex];
 
-    var message = `${greeting}, ${name}! ${humorousMessage}`;
+	// Construct the final message
+	var message = `${greeting}, ${humorousMessage}`;
 
-    // Display greeting message on the page
-    var greetingMessageElement = document.getElementById('greeting-message');
-    greetingMessageElement.textContent = message;
-    greetingMessageElement.classList.remove('hidden');
+	// Display greeting message on the page
+	var greetingMessageElement = document.getElementById('greeting-message');
+	greetingMessageElement.innerHTML = message; // Use innerHTML to render HTML content
+	greetingMessageElement.classList.remove('hidden');
 };
