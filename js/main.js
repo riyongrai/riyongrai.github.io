41c239dfd4a8d7f450d530fcb2ca0dd881cd3729
Riyong <rairiyong@gmail.com>
2024-07-04 11:43:06 -0400

    remove counter

--- js/main.js
+++ js/main.js
@@ -214,3 +214,113 @@ document.addEventListener("DOMContentLoaded", function() {
   setInterval(displayRandomSentence, 5000);
 
 
+// Tik tak TouchEvent
+const X_CLASS = 'x';
+const O_CLASS = 'o';
+const WINNING_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+const cellElements = document.querySelectorAll('[data-cell]');
+const board = document.getElementById('game');
+const winningMessageElement = document.getElementById('winningMessage');
+const winningMessageTextElement = document.getElementById('winningMessageText');
+const restartButton = document.getElementById('restartButton');
+const startGameButton = document.getElementById('startGameButton');
+const nameEntry = document.getElementById('nameEntry');
+const playerXNameInput = document.getElementById('playerXName');
+const playerONameInput = document.getElementById('playerOName');
+
+let oTurn;
+let playerXName = '';
+let playerOName = '';
+
+startGameButton.addEventListener('click', () => {
+    playerXName = playerXNameInput.value;
+    playerOName = playerONameInput.value;
+    if (playerXName && playerOName) {
+        nameEntry.classList.add('hide');
+        board.classList.remove('hide');
+        startGame();
+    } else {
+        alert('Please enter names for both players.');
+    }
+});
+
+restartButton.addEventListener('click', startGame);
+
+function startGame() {
+    oTurn = false;
+    cellElements.forEach(cell => {
+        cell.classList.remove(X_CLASS);
+        cell.classList.remove(O_CLASS);
+        cell.innerText = '';
+        cell.removeEventListener('click', handleClick);
+        cell.addEventListener('click', handleClick, { once: true });
+    });
+    setBoardHoverClass();
+    winningMessageElement.classList.remove('show');
+}
+
+function handleClick(e) {
+    const cell = e.target;
+    const currentClass = oTurn ? O_CLASS : X_CLASS;
+    placeMark(cell, currentClass);
+    if (checkWin(currentClass)) {
+        endGame(false);
+    } else if (isDraw()) {
+        endGame(true);
+    } else {
+        swapTurns();
+        setBoardHoverClass();
+    }
+}
+
+function endGame(draw) {
+    if (draw) {
+        winningMessageTextElement.innerText = 'Draw!';
+    } else {
+        winningMessageTextElement.innerText = `${oTurn ? playerOName : playerXName} Wins!`;
+    }
+    winningMessageElement.classList.add('show');
+}
+
+function isDraw() {
+    return [...cellElements].every(cell => {
+        return cell.classList.contains(X_CLASS) || cell.classList.contains(O_CLASS);
+    });
+}
+
+function placeMark(cell, currentClass) {
+    cell.classList.add(currentClass);
+    cell.innerText = currentClass.toUpperCase();
+}
+
+function swapTurns() {
+    oTurn = !oTurn;
+}
+
+function setBoardHoverClass() {
+    board.classList.remove(X_CLASS);
+    board.classList.remove(O_CLASS);
+    if (oTurn) {
+        board.classList.add(O_CLASS);
+    } else {
+        board.classList.add(X_CLASS);
+    }
+}
+
+function checkWin(currentClass) {
+    return WINNING_COMBINATIONS.some(combination => {
+        return combination.every(index => {
+            return cellElements[index].classList.contains(currentClass);
+        });
+    });
+}
